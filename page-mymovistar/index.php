cbbf553a259e4cc56768d2276a9f11427584728a
Alejandra Aravena Silva - Everis <alejandra.aravena.silva@everis.com>
2018-04-17 14:39:30 -0300

    correcion boton de prueba

--- page-mymovistar/index.php
+++ page-mymovistar/index.php
@@ -20,8 +20,8 @@
 
 <pre><?php var_dump($_GET); ?></pre>
 
-<form action="poculm://everis-ulm-poc.herokuapp.com/page-mymovistar/?prueba=OK">
-    <input type="submit" value="Go to Google" />
+<form action="poculm://everis-ulm-poc.herokuapp.com/page-mymovistar/?prueba=OK" method="get" >
+    <input type="submit" value="Ir a la app" />
 </form>
 
 </body>
