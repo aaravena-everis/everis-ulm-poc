cbe291d299336ac372fac378ba13921163c5155f
Alejandra Aravena Silva - Everis <alejandra.aravena.silva@everis.com>
2018-04-25 14:07:31 -0300

    prueba 6

--- page-mymovistar/index.php
+++ page-mymovistar/index.php
@@ -1,25 +1,9 @@
 <html>
 <head>
-	<script type="text/javascript">
-	var parts = location.href.split('#');
-	if(parts.length > 1)
-	{
-	    var params = parts[0].split('?');
-	    var mark = '?';
-	    if(params.length > 1)
-	    {
-	        mark = '&';
-	    }
-	    location.href = parts[0] + mark + 'fragment=' + parts[1];
-	}
-
-	</script>	
+	
 </head>
 <body onload="myFunction()">
 	<h1>Recibido JS</h1>
-<p> <?php echo 'Reenviando ' . htmlspecialchars($_GET['fragment']); ?> </p>
-
-<pre><?php var_dump($_GET); ?></pre>
 
 <p id="url"></p>
 
