68c42637b67a1c0a4d637a0a2c4e7beb329fb54a
Alejandra Aravena Silva - Everis <alejandra.aravena.silva@everis.com>
2018-04-25 13:33:59 -0300

    prueba 3

--- page-mymovistar/index.php
+++ page-mymovistar/index.php
@@ -1,30 +1,23 @@
 <html>
 <head>
 	<script type="text/javascript">
-	var parts = location.href.split('#');
-	if(parts.length > 1)
-	{
-	    var params = parts[0].split('?');
-	    var mark = '?';
-	    if(params.length > 1)
-	    {
-	        mark = '&';
-	    }
-	    location.href = parts[0] + mark + 'fragment=' + parts[1];
-	}
+
+	var getUrl = window.location;
 
 	function myFunction() {
-		window.location = "poculm://everis-ulm-poc.herokuapp.com/page-mymovistar/#"+window.location.href;
+		//window.location = "poculm://everis-ulm-poc.herokuapp.com/page-mymovistar/#";
 	}
 	</script>	
 </head>
 <body onload="myFunction()">
-	<h1>Recibido:</h1>
-<p> <?php echo 'Reenviando ' . htmlspecialchars($_GET['fragment']); ?> </p>
+	<h1>Recibido JS</h1>
 
-<pre><?php var_dump($_GET); ?></pre>
+<p id="url"></p>
 
 </body>
+<script>
+document.getElementById("url").innerHTML = 5getUrl;
+</script>
 
 
 </html>
