394b93320ca8a9c7ee4c27dfc5b8fae2457c08f1
Alejandra Aravena Silva - Everis <alejandra.aravena.silva@everis.com>
2018-04-25 12:12:33 -0300

    Reenvio para iOS

--- page-mymovistar/index.php
+++ page-mymovistar/index.php
@@ -13,22 +13,18 @@
 	    location.href = parts[0] + mark + 'fragment=' + parts[1];
 	}
 
+	console.log("----------FRAGMENT: "+parts[1]);
+
 	function myFunction() {
-		window.location = "poculm://everis-ulm-poc.herokuapp.com/page-mymovistar/?prueba=OK";
+		window.location = "poculm://everis-ulm-poc.herokuapp.com/page-mymovistar/#"+parts[1];
 	}
 	</script>	
 </head>
 <body onload="myFunction()">
 	<h1>Recibido:</h1>
-<p> <?php echo 'fragmento ' . htmlspecialchars($_GET['fragment']); ?> </p>
+<p> <?php echo 'Reenviando ' . htmlspecialchars($_GET['fragment']); ?> </p>
 
 <pre><?php var_dump($_GET); ?></pre>
-
-<a href="poculm://everis-ulm-poc.herokuapp.com/page-mymovistar/?prueba=OK" target="_blank">Ir a la app </a>
-
-<form action="poculm://everis-ulm-poc.herokuapp.com/page-mymovistar/?prueba=OK" target="_blank">
-        <input type="submit" value="Open App" />
-    </form>
 
 </body>
 
