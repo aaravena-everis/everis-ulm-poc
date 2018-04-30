0264d09b8a9af95deb904cf7e023a16fd8a5d8e1
Alejandra Aravena Silva - Everis <alejandra.aravena.silva@everis.com>
2018-04-30 12:11:41 -0300

    complejidad

--- page-mymovistar/index.php
+++ page-mymovistar/index.php
@@ -4,17 +4,39 @@
 </head>
 <body onload="myFunction()">
 	<h1>Recibido JS</h1>
-
+	<h2 id="res"></h2>
+<h3>Via #</h3>
 <p id="url"></p>
 
+<h3>Via ?</h3>
+<p id="url2"></p>
 </body>
 <script>
 	var getUrl = window.location.href;
 	getUrl = getUrl.split('#');
-document.getElementById("url").innerHTML = String(getUrl[1]);
+	document.getElementById("url").innerHTML = String(getUrl[1]);
+
+	var getUrl2 = window.location.href;
+	getUrl2 = getUrl2.split('?');
+	document.getElementById("url2").innerHTML = String(getUrl2[1]);
+
+	if (String(getUrl[1])) {
+		document.getElementById("res").innerHTML = "Recibido via #";
+	} else if (String(getUrl2[1])) {
+		document.getElementById("res").innerHTML = "Recibido via ?";
+	} else {
+		document.getElementById("res").innerHTML = "Nada Recibido"
+	}
 
 function myFunction() {
-	window.location = "poculm://everis-ulm-poc.herokuapp.com/page-mymovistar/?fragment="+String(getUrl[1]);
+	if (String(getUrl[1])) {
+		window.location = "poculm://everis-ulm-poc.herokuapp.com/page-mymovistar/?fragment="+String(getUrl[1]);
+	} else if (String(getUrl2[1])) {
+		window.location = "poculm://everis-ulm-poc.herokuapp.com/page-mymovistar/?fragment="+String(getUr2l[1]);
+	} else {
+		console.log("nada recibido");
+	}
+	
 }
 </script>
 
